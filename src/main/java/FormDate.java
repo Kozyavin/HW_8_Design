953714ebb56c988625baab123413c7c6d827965d
Kozyavin V <kioci@yandex.ru>
2023-05-01 18:29:24 +0300

    IntialCommit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,6 @@
+public class FormDate {
+    public int date;
+    public int month;
+    public int year;
+
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {   //класс с полями описывающими объект запроса на сервер
+
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
